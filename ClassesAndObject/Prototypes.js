edb43dd4f2a1e1abce864f52194e227d042b7bb0
suuraaj <suraaj,sinngh@gmail.com>
2024-01-10 11:59:06 +0530

    Adding Rock Paper Scissors Game

---
+++ ClassesAndObject/Prototypes.js
@@ -0,0 +1,7 @@
+//prototypes in JS
+//A javascript object is an entity state and behavioe(properties and method)
+
+//JS object have a special property called prototype.
+
+//
+//if object and prototype have same method object's method will be used
